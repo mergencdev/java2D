f720eb9778882464516b25fbb2a80ff997674376
Mehmet Emin Ergenç <mehmet.ergenc@appcent.mobi>
2021-12-23 19:32:13 +0300

    show shapes and fills on frame

--- 02ShapesAndFills/src/com/company/Main.java
+++ 02ShapesAndFills/src/com/company/Main.java
@@ -1,15 +1,20 @@
 package com.company;
 
+import javax.swing.*;
 import java.awt.*;
 
-public class Main {
+public class Main extends JFrame {
 
     Main() {
         initUI();
     }
 
     private void initUI() {
-
+        add(new Surface());
+        setTitle("Basic shapes");
+        setSize(350, 250);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
     public static void main(String[] args) {
@@ -17,7 +22,7 @@ public class Main {
             @Override
             public void run() {
                 Main shapesAndFillsExample = new Main();
-
+                shapesAndFillsExample.setVisible(true);
             }
         });
     }
